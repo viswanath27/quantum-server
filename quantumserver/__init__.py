5b01a635b3fd59559905bb998c1bba188adf8c9d
Viswanath.Barenkala <code.vissu@gmail.com>
2019-09-06 14:50:10 +0530

    db issue solved

--- quantumserver/__init__.py
+++ quantumserver/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8bedc6ccbfbb7d71d2654b1fa280189a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quantumDatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/quantumDatabase.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
